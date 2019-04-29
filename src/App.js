3408bdf011fab81047368e167f31db9cddb7448d
yaxueguo <yaxue@live.unc.edu>
2019-04-29 15:52:14 -0400

    add navbar in sync with other subcomponents at the same level

--- src/App.js
+++ src/App.js
@@ -41,7 +41,9 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <Navbar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
           <Counters
             counters={this.state.counters}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,9 @@ class Navbar extends Component {
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
