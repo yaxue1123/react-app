0ea52eef8646fe552512a05c74f4779e7165affb
yaxueguo <yaxue@live.unc.edu>
2019-05-01 05:14:12 -0400

    destruct arguments in Table component

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,21 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import TableBody from "./tableBody";
 import TableHeader from "./tableHeader";
 
-class Table extends Component {
-  render() {
-    const { columns, onSort, sortColumn, data } = this.props;
-    return (
-      <table className="table">
-        <TableHeader
-          columns={columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody columns={columns} data={data} />
-      </table>
-    );
-  }
-}
+const Table = ({ columns, onSort, sortColumn, data }) => {
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody columns={columns} data={data} />
+    </table>
+  );
+};
 
 export default Table;
