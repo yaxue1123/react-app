760d133dd7139aec9883defda8a333e0283f086b
yaxueguo <yaxue@live.unc.edu>
2019-05-26 02:41:02 -0400

    add react router and routes

--- src/App.js
+++ src/App.js
@@ -1,67 +1,24 @@
 import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-
-    this.setState({ counters });
-  };
-
-  handleDelete = counterID => {
-    const counters = this.state.counters.filter(c => c.id !== counterID);
-
-    this.setState({ counters });
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    // don't touch original state.
-    // clone the element to be incremented.
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDecrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
-  };
-
   render() {
     return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
-        />
-        <main className="container">
-          <Counters
-            counters={this.state.counters}
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-          />
-        </main>
-      </React.Fragment>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
     );
   }
 }
---
+++ src/components/rentals.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Rentals = () => {
+  return <h1>Page Not Found</h1>;
+};
+
+export default Rentals;
