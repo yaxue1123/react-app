6f7d7c5b85d751569f08165be13283573459121b
yaxueguo <yaxue@live.unc.edu>
2019-05-26 15:57:23 -0400

    handle multiple inputs & controlled elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,39 +1,47 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  // create a ref object.
-  username = React.createRef();
-
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  state = { account: { username: "", password: "" } };
 
   handleSubmit = e => {
     e.preventDefault();
+    console.log("submitted");
+  };
 
-    // Call the server
-    const username = this.username.current.value;
-    console.log("submitted", username);
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Log in</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <lable htmlFor="username">Username</lable>
+            <label htmlFor="username">Username</label>
             <input
               autoFocus
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
-            <lable htmlFor="password">Password</lable>
-            <input id="password" type="text" className="form-control" />
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
