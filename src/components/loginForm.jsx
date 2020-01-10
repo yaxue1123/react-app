aaff079c9fc2bb70d2593d3fc34ad8b885590420
yaxue1123 <yaxue@live.unc.edu>
2020-01-10 15:36:10 -0500

    extracted a reusable input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
+import Input from './common/input';
 
 class LoginForm extends Component {
   state = { account: { username: "", password: "" } };
 
   handleSubmit = e => {
+    // Prevent page reload.
     e.preventDefault();
+    // Call the server.
     console.log("submitted");
   };
 
   handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account }); 
   };
 
   render() {
@@ -20,29 +23,18 @@ class LoginForm extends Component {
       <div>
         <h1>Log in</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              value={account.username}
-              onChange={this.handleChange}
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input 
+            name="username" 
+            value={account.username} 
+            label="Username" 
+            onChange={this.handleChange} 
+          />
+          <Input 
+            name="password" 
+            value={account.password} 
+            label="Password" 
+            onChange={this.handleChange} 
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
