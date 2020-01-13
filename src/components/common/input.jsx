e2fbc5d3b5b4a5eae599dc6d27b1062093081898
yaxue1123 <yaxue@live.unc.edu>
2020-01-13 16:24:30 -0500

    used spread operator to simply code

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Input = ({ type, name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
     return (
         <div className="form-group">
         <label htmlFor={name}>{label}</label>
         <input
-          value={value}
-          onChange={onChange}
-          type={type}
+          {...rest}
           id={name}
           name={name}
           className="form-control"
