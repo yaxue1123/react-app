329131912f1f214f2a8ed0db70f93e422e10b814
yaxue1123 <yaxue@live.unc.edu>
2020-02-14 12:46:11 -0500

    hide the delete col of movie form when not admin user or not logged in

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Table from "./common/table";
 import Like from "./common/like";
+import auth from "../services/authService";
 
 class MoviesTable extends Component {
   columns = [
@@ -19,18 +20,26 @@ class MoviesTable extends Component {
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
     },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      )
-    }
   ];
+
+  deletedColumn = {
+    key: "delete",
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) 
+      this.columns.push(this.deletedColumn);
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
